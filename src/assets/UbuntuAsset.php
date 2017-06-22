311b372b7df402db43331dd46312a0d89bc8e6b0
Vitaliy <theyamshikov@yandex.ru>
2017-06-22 23:32:27 +0600

    init files

---
+++ src/assets/UbuntuAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zein
+ * Date: 8/2/14
+ * Time: 11:40 AM
+ */
+
+namespace yii2lab\ubuntu_font;
+
+use yii\web\AssetBundle;
+
+class UbuntuAsset extends AssetBundle
+{
+	public $sourcePath = '@yii2lab/ubuntu_font/dist';
+	public $css = [
+		'styles.css',
+	];
+	private $addonCss = '';
+	
+	function init() {
+		parent::init();
+	}
+	
+}
